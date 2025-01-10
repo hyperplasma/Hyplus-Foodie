bd6dc3dc2ba2acfd2d4d79c5437796d9e682213d
Akira37 <863781751@qq.com>
2025-01-10 15:37:21 +0800

    Add websocket config

--- hyplus-foodie-spring/hyplus-server/src/main/java/top/hyperplasma/task/MyTask.java
+++ hyplus-foodie-spring/hyplus-server/src/main/java/top/hyperplasma/task/MyTask.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 
 /**
- * 自定义定时任务类
+ * 自定义定时任务类示例
  */
-@Component
+// @Component
 @Slf4j
 public class MyTask {
 
     /**
      * 定时任务 每5秒触发一次
      */
-    @Scheduled(cron = "0/5 * * * * ?")
+    // @Scheduled(cron = "0/5 * * * * ?")
     public void executeTask() {
         log.info("定时任务开始执行：{}", new Date());
     }
